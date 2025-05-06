5575e9c61ad497344295d8cb43454f8c219d1059
prem-thatikonda29 <2023.tprem@isu.ac.in>
2025-05-06 12:42:12 +0530

    made css the default option when the code preview modal is opened

--- src/pages/Preview.tsx
+++ src/pages/Preview.tsx
@@ -183,6 +183,12 @@ module.exports = {
     }
   };
 
+  useEffect(() => {
+    if (isPreviewOpen) {
+      handleExport("css");
+    }
+  }, [isPreviewOpen]);
+
   return (
     <div className="flex flex-col min-h-screen">
       <div className="fixed top-0 left-0 right-0 z-50">
